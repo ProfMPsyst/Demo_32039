5243526690713b6c171142ef7395c3714a882f81
megha <megha.patel2@sheridancollege.ca>
2023-05-18 14:09:33 -0400

    my first commit

---
+++ src/greetingmsg/Welcome.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package greetingmsg;
+
+/**
+ *
+ * @author Megha Patel,2023
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
