386077d2d99c01703d91d47f71131013fdf66a25
ProfMPsyst <65903834+ProfMPsyst@users.noreply.github.com>
2023-05-18 14:54:20 -0400

    Update Welcome.java
    print line added

--- src/greetingmsg/Welcome.java
+++ src/greetingmsg/Welcome.java
@@ -16,6 +16,8 @@ public class Welcome {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("Welcpem to demo of Git);
+                           
     }
 
 }
